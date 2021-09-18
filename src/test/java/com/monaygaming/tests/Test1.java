fdb9a407c7ed6b9f8f6f99476a299aa67f7f2a05
kzmsrky87 <kzmsrky87@outlook.com>
2021-09-18 17:07:07 +0200

    Added webdriverfactory

--- src/test/java/com/monaygaming/tests/Test1.java
+++ src/test/java/com/monaygaming/tests/Test1.java
@@ -1,14 +1,19 @@
 package com.monaygaming.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Test1 {
     @BeforeMethod
-    public void setup(){
 
-    }
+
+
+
 
     @Test
     public void test1(){
---
+++ src/test/java/com/monaygaming/utilities/web.java
@@ -0,0 +1,23 @@
+package com.monaygaming.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class web { public static WebDriver getDriver(String browserType){
+    WebDriver driver=null;
+    switch (browserType.toLowerCase()){
+        case  "chrome" :
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            break;
+        case  "edge" :
+            WebDriverManager.edgedriver().setup();
+            driver= new EdgeDriver();
+            break;
+
+    }
+    return driver;
+}
+}
